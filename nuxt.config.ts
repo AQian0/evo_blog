663b02ed93a2375da22aaaa917586efd833bc3cf
AQian <970506352@qq.com>
2023-08-15 00:37:51 +0800

    feat: 引入vueuse

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ['@nuxtjs/eslint-module', '@unocss/nuxt'],
+  modules: ['@nuxtjs/eslint-module', '@unocss/nuxt', '@vueuse/nuxt'],
   css: ['@unocss/reset/tailwind.css'],
   devtools: { enabled: true },
 });
