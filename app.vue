6619a3431aacc734eb69ce8982bdeb30297bf64e
AQian <970506352@qq.com>
2024-02-29 16:14:20 +0800

    feat: 实现菜单跳转

--- app.vue
+++ app.vue
@@ -2,9 +2,73 @@
   <div min-h-screen w-full flex bg-zinc-100>
     <Nav overflow-hidden></Nav>
     <NuxtPage class="flex-[3]"></NuxtPage>
+    <div fixed top-4 right-6 text-6 z-10 opacity-50>
+      <Transition name="fade" mode="out-in">
+        <div v-if="!isMenuExpand">
+          <div
+            @click="menuIcons[0].method"
+            :class="menuIcons[0].label"
+            cursor-pointer
+          ></div>
+        </div>
+        <div space-y-1 v-else>
+          <div
+            v-for="menuIcon in menuIcons.slice(1)"
+            @click="menuIcon.method"
+            :class="menuIcon.label"
+            cursor-pointer
+          ></div>
+        </div>
+      </Transition>
+    </div>
   </div>
 </template>
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+const router = useRouter();
+const isMenuExpand = ref(false);
+const menuIcons = ref([
+  {
+    label: 'i-ri-menu-fill',
+    method: () => {
+      isMenuExpand.value = true;
+    },
+  },
+  {
+    label: 'i-ri-close-circle-line',
+    method: () => {
+      isMenuExpand.value = false;
+    },
+  },
+  {
+    label: 'i-ri-home-8-line',
+    method: () => {
+      router.push('/');
+    },
+  },
+  {
+    label: 'i-ri-file-list-line',
+    method: () => {
+      router.push({
+        path: '/',
+        query: {
+          type: 'article',
+        },
+      });
+    },
+  },
+  {
+    label: 'i-ri-book-2-line',
+    method: () => {
+      router.push({
+        path: '/',
+        query: {
+          type: 'book',
+        },
+      });
+    },
+  },
+]);
+</script>
 <style>
 .page-enter-active,
 .page-leave-active {
@@ -14,5 +78,15 @@
 .page-leave-to {
   opacity: 0;
   filter: blur(1rem);
+}
+
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s ease;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
 }
 </style>
