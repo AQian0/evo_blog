c2124bacf117d0cf474dbc50c36019a1c4e49d15
AQian <970506352@qq.com>
2024-02-29 17:07:42 +0800

    feat: 抽离menu组件并调整路由跳转方式

--- app.vue
+++ app.vue
@@ -3,73 +3,10 @@
     <Nav overflow-hidden></Nav>
     <NuxtLoadingIndicator :height="1"></NuxtLoadingIndicator>
     <NuxtPage class="flex-[3]"></NuxtPage>
-    <div fixed top-4 right-6 text-6 z-10 opacity-50>
-      <Transition name="fade" mode="out-in">
-        <div v-if="!isMenuExpand">
-          <div
-            @click="menuIcons[0].method"
-            :class="menuIcons[0].label"
-            cursor-pointer
-          ></div>
-        </div>
-        <div space-y-1 v-else>
-          <div
-            v-for="menuIcon in menuIcons.slice(1)"
-            @click="menuIcon.method"
-            :class="menuIcon.label"
-            cursor-pointer
-          ></div>
-        </div>
-      </Transition>
-    </div>
+    <BaseMenu></BaseMenu>
   </div>
 </template>
-<script lang="ts" setup>
-const router = useRouter();
-const isMenuExpand = ref(false);
-const menuIcons = ref([
-  {
-    label: 'i-ri-menu-fill',
-    method: () => {
-      isMenuExpand.value = true;
-    },
-  },
-  {
-    label: 'i-ri-close-circle-line',
-    method: () => {
-      isMenuExpand.value = false;
-    },
-  },
-  {
-    label: 'i-ri-home-8-line',
-    method: () => {
-      router.push('/');
-    },
-  },
-  {
-    label: 'i-ri-file-list-line',
-    method: () => {
-      router.push({
-        path: '/',
-        query: {
-          type: 'article',
-        },
-      });
-    },
-  },
-  {
-    label: 'i-ri-book-2-line',
-    method: () => {
-      router.push({
-        path: '/',
-        query: {
-          type: 'book',
-        },
-      });
-    },
-  },
-]);
-</script>
+<script lang="ts" setup></script>
 <style>
 .page-enter-active,
 .page-leave-active {
---
+++ components/Base/menu.vue
@@ -0,0 +1,67 @@
+<template>
+  <div fixed top-4 right-6 text-6 z-10 opacity-50>
+    <Transition name="fade" mode="out-in">
+      <div v-if="!isMenuExpand">
+        <div
+          @click="menuIcons[0].method"
+          :class="menuIcons[0].label"
+          cursor-pointer
+        ></div>
+      </div>
+      <div space-y-1 v-else>
+        <div
+          v-for="menuIcon in menuIcons.slice(1)"
+          @click="menuIcon.method"
+          :class="menuIcon.label"
+          cursor-pointer
+        ></div>
+      </div>
+    </Transition>
+  </div>
+</template>
+<script lang="ts" setup>
+const isMenuExpand = ref(false);
+const menuIcons = ref([
+  {
+    label: 'i-ri-menu-fill',
+    method: () => {
+      isMenuExpand.value = true;
+    },
+  },
+  {
+    label: 'i-ri-close-circle-line',
+    method: () => {
+      isMenuExpand.value = false;
+    },
+  },
+  {
+    label: 'i-ri-home-8-line',
+    method: async () => {
+      await navigateTo('/');
+    },
+  },
+  {
+    label: 'i-ri-file-list-line',
+    method: async () => {
+      await navigateTo({
+        path: '/',
+        query: {
+          type: 'article',
+        },
+      });
+    },
+  },
+  {
+    label: 'i-ri-book-2-line',
+    method: async () => {
+      await navigateTo({
+        path: '/',
+        query: {
+          type: 'book',
+        },
+      });
+    },
+  },
+]);
+</script>
+<style scoped></style>
