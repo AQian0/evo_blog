b321bff0f8276f6d605387e3fdd236ff470d1c31
AQian <970506352@qq.com>
2024-05-17 11:18:12 +0800

    fix: 修复Transition组件无法及时应用样式的错误

--- components/Base/theme-button.vue
+++ components/Base/theme-button.vue
@@ -1,16 +1,10 @@
 <template>
-  <Transition
-    name="fade"
-    mode="out-in"
-    cursor-pointer
-    opacity-50
-    transition
-    hover:opacity-100
-    @click="toggleDark(!isDark)"
-  >
-    <div v-if="!isDark" i-ri-sun-fill />
-    <div v-else i-ri-moon-fill />
-  </Transition>
+  <div cursor-pointer opacity-50 transition hover:opacity-100>
+    <Transition name="fade" mode="out-in" @click="toggleDark(!isDark)">
+      <div v-if="!isDark" i-ri-sun-fill />
+      <div v-else i-ri-moon-fill />
+    </Transition>
+  </div>
 </template>
 <script lang="ts" setup>
 const isDark = useDark();
