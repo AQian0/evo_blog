ca58a83b6e7a5dc362d511aa822e43e66c828d08
AQian <970506352@qq.com>
2024-05-20 11:35:37 +0800

    refactor: 重构分类组件

---
+++ components/Base/tabs.vue
@@ -0,0 +1,28 @@
+<template>
+  <div flex gap-6 font-bold>
+    <span
+      v-for="tab in props.tabs"
+      cursor-pointer
+      text-6
+      transition
+      hover:opacity-80
+      :class="selectedValue === tab.value ? 'opacity-80' : 'opacity-40'"
+      @click="select(tab.value)"
+      >{{ tab.label }}</span
+    >
+  </div>
+</template>
+<script lang="ts" setup>
+type Tab = {
+  label: number | string;
+  value: number | string;
+};
+const selectedValue = defineModel<number | string>();
+const props = defineProps<{
+  tabs: Tab[];
+}>();
+const select = (value: number | string) => {
+  selectedValue.value = value;
+};
+</script>
+<style scoped></style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,6 @@
 <template>
-  <div flex="~ col" items-center justify-around gap-4>
+  <div flex="~ col" relative items-center justify-around gap-4>
+    <BaseTabs v-model="type" :tabs="tabs" absolute left-6 top--18 blur-0.3 />
     <home-posts>
       <home-posts-post v-for="post in posts" :post="post" />
     </home-posts>
@@ -22,7 +23,36 @@ const pageSize = ref(10);
 const changePage = (page: number) => {
   currentPage.value = page;
 };
-
+const type = ref((route.query.type as string | undefined) || '');
+watch(
+  () => type.value,
+  async () => {
+    if (type.value) {
+      await navigateTo({
+        path: '/',
+        query: {
+          type: type.value,
+        },
+      });
+    } else {
+      await navigateTo('/');
+    }
+  },
+);
+const tabs = ref([
+  {
+    label: '全部',
+    value: '',
+  },
+  {
+    label: '文章',
+    value: 'article',
+  },
+  {
+    label: '书籍',
+    value: 'book',
+  },
+]);
 const { data: total } = await useAsyncData('total', async () => {
   const { count, error } = await client
     .from('post')
