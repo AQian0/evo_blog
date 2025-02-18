a5e02c24499bbc1f0e189501907b8ed810339c8b
AQian <970506352@qq.com>
2025-02-18 17:28:15 +0800

    chore: 调整props调用方式

--- components/home/posts/post.vue
+++ components/home/posts/post.vue
@@ -4,9 +4,9 @@
     @click="goPost"
   >
     <div class="flex justify-between text-xl">
-      <base-text omitted>{{ props.post.title }}</base-text>
+      <base-text omitted>{{ post?.title }}</base-text>
       <base-text omitted>{{
-        useDateText(props.post.updatedAt)
+        useDateText(post?.updatedAt)
       }}</base-text>
     </div>
     <base-text omitted>
@@ -15,19 +15,28 @@
   </li>
 </template>
 <script lang="ts" setup>
-const props = defineProps();
+const {
+  post
+} = defineProps<{
+  post: {
+    postId: number
+    title: string
+    updatedAt: string
+    content: string
+  }
+}>();
 const extractText = computed(() => {
-  return (props.post.content as string)
+  return (post?.content)
     .replace(/<img\b[^>]*>/gi, '[图片]')
     .replace(/<[^>]+>/g, '')
     .substring(0, 100);
 });
 const goPost = async () => {
-  if (!props.post.id) return;
+  if (!post?.postId) return;
   await navigateTo({
     path: '/post',
     query: {
-      id: props.post.id,
+      id: post.postId,
     },
   });
 };
