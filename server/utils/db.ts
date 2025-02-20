fe9c12adfa3ce99df9436f098315022a2bace6c0
AQian <970506352@qq.com>
2025-02-20 15:06:36 +0800

    chore: 调整调用方式

--- server/utils/db.ts
+++ server/utils/db.ts
@@ -1,8 +1,7 @@
 import 'dotenv/config';
 import { drizzle } from 'drizzle-orm/postgres-js';
 
-
-export const useDrizzle = drizzle({
+export const useDrizzle = () => drizzle({
   connection: process.env.DATABASE_URL!,
   casing: 'snake_case',
 });
