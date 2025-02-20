95d342186219d9dbdb9875e1a8e66489a6b12072
AQian <970506352@qq.com>
2025-02-20 15:05:38 +0800

    feat: 实现分页请求头添加方法

---
+++ server/utils/pagination.ts
@@ -0,0 +1,11 @@
+import type { H3Event, EventHandlerRequest } from 'h3'
+
+export const appendPagination = (event: H3Event<EventHandlerRequest>, totalCount: number) => {
+  const { page = 1, perPage = 10 }: { page: number, perPage: number} = getQuery(event)
+  appendResponseHeaders(event, {
+    "X-Page": page,
+    "X-Per-Page": perPage,
+    "X-Total-Count": totalCount,
+    "X-Total-Pages": (totalCount / perPage) + 1,
+  })
+}
