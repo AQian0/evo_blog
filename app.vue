6cd82379788c36172729362fcfa47e082b3a87c5
AQian <970506352@qq.com>
2025-02-21 15:13:23 +0800

    feat: 实现全局滚动条

--- app.vue
+++ app.vue
@@ -1,17 +1,19 @@
 <template>
-  <div class="min-h-screen w-full flex bg-stone-100">
-    <Nav />
-    <NuxtLoadingIndicator :height="1" />
-    <div class="flex flex-col flex-3 relative items-center py-20">
-      <BasePageHeader />
-      <NuxtPage class="w-full flex-1" />
-      <Footer />
+  <OverlayScrollbarsComponent defer class="h-screen bg-stone-100">
+    <div class="min-h-screen w-full flex overflow-hidden">
+        <Nav />
+        <NuxtLoadingIndicator :height="1" />
+        <div class="flex flex-col flex-3 relative items-center py-16">
+          <BasePageHeader />
+          <NuxtPage class="w-full flex-1" />
+          <Footer />
+        </div>
+        <div class="fixed right-6 top-4 z-10 flex items-center gap-4 text-2xl">
+          <BaseSearch />
+          <BaseThemeButton />
+        </div>
     </div>
-    <div class="fixed right-6 top-4 z-10 flex items-center gap-4 text-2xl">
-      <BaseSearch />
-      <BaseThemeButton />
-    </div>
-  </div>
+  </OverlayScrollbarsComponent>
 </template>
 <script lang="ts" setup></script>
 <style>
