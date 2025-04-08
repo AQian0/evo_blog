769d102a66ef9ec8ff8180f926740d5ebe8a1055
AQian <970506352@qq.com>
2025-04-08 09:53:27 +0800

    feat: 实现全文搜索

--- server/api/blogs/index.get.ts
+++ server/api/blogs/index.get.ts
@@ -1,6 +1,21 @@
+import Fuse from 'fuse.js'
+
 export default defineEventHandler(async (event) => {
-  const { page = 1, perPage = 10 }: { page: number, perPage: number} = getQuery(event)
-  const totalCount = await queryCollection(event, 'blogs').count()
+  const { page = 1, perPage = 10, search = '' }: { page: number, perPage: number, search: string } = getQuery(event)
+  let builder = queryCollection(event, 'blogs')
+  const totalCount = await builder.count()
   appendPagination(event, totalCount)
-  return await queryCollection(event, 'blogs').skip((page - 1) * perPage).limit(perPage).all()
+  // 或许应该使用id作为filter
+  const blogTitles: Array<string> = []
+  if (search) {
+    const blogs = await queryCollectionSearchSections(event, 'blogs')
+    const fuse = new Fuse(blogs, {
+      keys: ['title', 'tag']
+    })
+    blogTitles.push(...fuse.search(search).map(item => item.item.title))
+  }
+  if (blogTitles.length) {
+    builder = builder.where('title', 'IN', blogTitles)
+  }
+  return await builder.skip((page - 1) * perPage).limit(perPage).all()
 })
