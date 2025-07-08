f7646a25af239b9cc9908141cd145bab09f210b6
AQian <970506352@qq.com>
2025-07-08 10:58:48 +0800

    chore: 移除无用drizzle内容

--- server/utils/db.ts
+++
@@ -1,7 +0,0 @@
-import 'dotenv/config';
-import { drizzle } from 'drizzle-orm/postgres-js';
-
-export const useDrizzle = () => drizzle({
-  connection: process.env.DATABASE_URL!,
-  casing: 'snake_case',
-});
