694a81f30c4fac9df02379628ea77ca646f86211
AQian <970506352@qq.com>
2025-07-12 09:39:32 +0800

    perf: 使用motion-v替换原有动画

--- app/components/content/CodePanel.vue
+++ app/components/content/CodePanel.vue
@@ -3,14 +3,17 @@
     v-model:open="open"
     class="flex flex-col my-2 gap-2"
   >
-    <button class="flex p-1 justify-center items-center hover:bg-zinc-200 transition-all rounded-sm">
-      <div class="flex justify-center items-center gap-1 flex-none">
-        <Icon size="1rem" :name></Icon>
+    <button
+      class="flex p-1 items-center hover:bg-zinc-200 rounded-sm"
+      :class="open ? 'justify-between' : 'justify-center'"
+    >
+      <Motion as="div" class="flex justify-center items-center gap-1 flex-none" layout :transition="{ type: 'tween' }">
+        <Icon size="1rem" :name />
         <div class="text-base">{{ fileName }}</div>
-      </div>
-      <div class="transition-all ease-out duration-200 flex justify-end" :class="open ? 'flex-1' : ''">
-        <Icon :class="open ? '-rotate-90' : ''" size="1rem" name="i-ri-arrow-left-s-fill"></Icon>
-      </div>
+      </Motion>
+      <Motion as="div" class="leading-0" :animate="{ rotate: open ? -90 : 0 }" layout :transition="{ type: 'tween' }">
+        <Icon size="1.5rem" name="i-ri-arrow-left-s-fill" />
+      </Motion>
     </button>
     <template #content>
       <div class="p-2 bg-zinc-200 rounded-sm">
