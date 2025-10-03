a4475d6f09b420d0eacc030d556f4bfd9e420bd5
AQian <970506352@qq.com>
2025-10-04 02:29:00 +0800

    refactor: 调整组件名，移除显示逻辑

--- app/components/Base/back.vue
+++ app/components/Base/back.vue
@@ -1,22 +1,29 @@
 <template>
   <div
-    v-show="!isRoot"
     class="absolute left-6 top-4 z-10 flex cursor-pointer items-center opacity-50 transition space-x-1 hover:opacity-100"
-    @click="goBack"
+    @click="back"
   >
     <button class="leading-0">
       <Icon size="2rem" name="i-ri-arrow-left-double-fill" />
     </button>
-    <div>返回</div>
+    <div>{{ text }}</div>
   </div>
 </template>
 <script lang="ts" setup>
-const route = useRoute();
+type Props = {
+  text?: string;
+  onBack?: () => void;
+};
+
+const { text = '返回', onBack } = defineProps<Props>();
+
 const router = useRouter();
-const isRoot = computed(() => {
-  return '/' === route.path;
-});
-const goBack = () => {
+
+const back = (): void => {
+  if (onBack) {
+    onBack();
+    return;
+  }
   router.back();
 };
 </script>
