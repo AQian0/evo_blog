6d320c9418aaed0e3877a8b11251239d4d47dc58
AQian <970506352@qq.com>
2025-10-04 02:34:33 +0800

    feat: 启用默认布局

--- app/app.vue
+++ app/app.vue
@@ -1,15 +1,8 @@
 <template>
   <OverlayScrollbarsComponent defer class="h-screen bg-stone-100">
     <NuxtLoadingIndicator :height="1" />
-    <div class="min-h-screen w-full flex overflow-hidden">
-        <Nav />
-        <div class="flex flex-col flex-3 relative items-center">
-          <BasePageHeader />
-          <NuxtPage class="w-full flex-1" />
-          <Footer />
-        </div>
-    </div>
+    <NuxtLayout>
+      <NuxtPage class="w-full flex-1" />
+    </NuxtLayout>
   </OverlayScrollbarsComponent>
 </template>
-<script lang="ts" setup></script>
-<style></style>
---
+++ app/layouts/default.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="min-h-screen w-full flex overflow-hidden">
+    <Nav />
+    <div class="flex flex-col flex-4 relative items-center">
+      <BaseBack v-if="route.path !== '/'" />
+      <slot />
+      <Footer />
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+const route = useRoute()
+</script>
