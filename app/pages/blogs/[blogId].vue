72dbf717537d3c2747498270b33c8d9bd35552d1
AQian <970506352@qq.com>
2025-10-07 04:45:30 +0800

    chore: 调整文本宽度

--- app/pages/blogs/[blogId].vue
+++ app/pages/blogs/[blogId].vue
@@ -2,10 +2,7 @@
   <div class="flex flex-col items-center">
     <div class="fixed left-0 top-0 z-1 h-40 w-full bg-gradient-to-t" />
     <div class="mt-24 indent-2 text-4xl tracking-[0.5rem]">{{ blog?.title }}</div>
-    <template v-if="blog">
-      <ContentRenderer class="relative my-6 w-192 prose prose-zinc" :value="blog.body" />
-    </template>
-    <!-- 未来补充骨架屏 -->
+    <ContentRenderer v-if="blog" class="relative my-6 w-1/2 max-w-none prose prose-zinc" :value="blog.body" />
     <div class="flex flex-col fixed transition space-y-4 left-[86%] top-[70%]">
       <button
         v-for="item in extendedList"
