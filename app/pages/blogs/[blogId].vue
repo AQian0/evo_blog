819734b17a394d7d12e5e246140a23828771f8bb
AQian <970506352@qq.com>
2025-12-16 01:57:16 +0800

    fix: 修复渲染props错误

--- app/pages/blogs/[blogId].vue
+++ app/pages/blogs/[blogId].vue
@@ -2,7 +2,7 @@
   <div class="flex flex-col items-center">
     <div class="fixed left-0 top-0 z-1 h-40 w-full bg-gradient-to-t" />
     <div class="mt-24 indent-2 text-4xl tracking-[0.5rem]">{{ blog?.title }}</div>
-    <ContentRenderer v-if="blog" class="relative my-6 w-1/2 max-w-none prose prose-zinc" :value="blog.body" />
+    <ContentRenderer v-if="blog" class="relative my-6 w-1/2 max-w-none prose prose-zinc" :value="blog" />
     <div class="flex flex-col fixed transition space-y-4 left-[86%] top-[70%]">
       <button
         v-for="item in extendedList"
