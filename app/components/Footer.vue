1ba5c51bd41051f8f22e57feebbfce8f40ba9181
AQian <970506352@qq.com>
2026-02-10 11:19:07 +0800

    chore: 移除注释

--- app/components/Footer.vue
+++ app/components/Footer.vue
@@ -1,8 +1,5 @@
 <template>
   <div class="mt-auto flex h-20 items-center">
-    <!-- 令人迷惑的一点——外层Unocss设置的透明度不会和内层的实现互动
-    因此内层的hover效果并不会生效
-    所以无奈地在每一个元素中添加了透明度 -->
     <div class="flex items-center gap-4 font-bold">
       <NuxtLink
         to="https://creativecommons.org/licenses/by-nc/4.0/deed.zh-hans"
