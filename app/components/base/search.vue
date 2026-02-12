5c75dd62078f8ecc0d823c87440f0b1e89864656
AQian <970506352@qq.com>
2026-02-12 16:53:01 +0800

    feat: 实现搜索栏Kbd绑定

--- app/components/base/search.vue
+++ app/components/base/search.vue
@@ -1,19 +1,36 @@
 <template>
   <div class="relative w-3xl text-base" @keyup.enter="search">
-    <Input v-model="content" type="text" :placeholder="placeholder" />
-    <button
-      type="button"
-      class="absolute top-[50%] right-3 translate-y-[-50%] cursor-pointer leading-0 opacity-50 transition hover:opacity-100"
-      @click="search"
-    >
-      <Icon name="i-ri-search-line" />
-    </button>
+    <Input ref="inputRef" v-model="content" type="text" :placeholder="placeholder" />
+    <div class="absolute top-[50%] right-3 translate-y-[-50%] flex items-center gap-2">
+      <KbdGroup>
+        <Kbd>Ctrl</Kbd>
+        <span>+</span>
+        <Kbd>K</Kbd>
+      </KbdGroup>
+      <button
+        type="button"
+        class="cursor-pointer leading-0 opacity-50 transition hover:opacity-100"
+        @click="search"
+      >
+        <Icon name="i-ri-search-line" />
+      </button>
+    </div>
   </div>
 </template>
 <script lang="ts" setup>
 const placeholder = ref("在此搜索");
 const route = useRoute();
 const content = ref((route.query.search as string) || "");
+
+const inputRef = ref<{ $el: HTMLInputElement } | null>(null);
+
+onKeyStroke("k", (e) => {
+  if (e.ctrlKey || e.metaKey) {
+    e.preventDefault();
+    inputRef.value?.$el.focus();
+  }
+});
+
 const search = async (): Promise<void> => {
   await navigateTo({
     path: "/",
